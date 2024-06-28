cfd1e7d6aba3439916080d49c7bb7638c487afb0
iNa. <aljard@naver.com>
2024-06-28 18:38:40 +0900

    240619_Context,useMemo,useCallback

--- basic/src/index.js
+++ basic/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppCard from './AppCard';
+import AppMentorsButton from './AppMentorsButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppCard />
+    <AppMentorsButton />
   </React.StrictMode>
 );
 
